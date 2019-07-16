b492705c7ae3d76fb43ab117b3ed7d63b32a941e
Fantom TM <QWERTYkez@outlook.com>
2019-07-16 22:59:12 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PM = OMineManager.ProfileManager;
+
+namespace OMineManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = PM.Profile;
+
+            PM.Initialize();
+
+            GPUsCB.ItemsSource = new string[] { "Auto", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
+            if (PM.Profile.GPUsSwitch != null)
+            { GPUsCB.SelectedIndex = PM.Profile.GPUsSwitch.Length; }
+            else { GPUsCB.SelectedIndex = 0; }
+        }
+
+        #region GPUs
+        private void GPUsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GPUswitchSP.Children.Clear();
+
+            string Selected = (string)GPUsCB.SelectedItem;
+            if (Selected == "Auto")
+            {
+                GPUsSwitchHeader.Visibility = Visibility.Hidden;
+                PM.Profile.GPUsSwitch = null;
+            }
+            else
+            {
+                GPUsSwitchHeader.Visibility = Visibility.Visible;
+                byte k = Convert.ToByte(Selected);
+                if (PM.Profile.GPUsSwitch == null)
+                {
+                    PM.Profile.GPUsSwitch = new bool[k];
+                    for (int i = 0; i < k; i++)
+                    {
+                        PM.Profile.GPUsSwitch[i] = true;
+                    }
+                }
+                else if (PM.Profile.GPUsSwitch.Length != k)
+                {
+                    PM.Profile.GPUsSwitch = new bool[k];
+                    for (int i = 0; i < k; i++)
+                    {
+                        PM.Profile.GPUsSwitch[i] = true;
+                    }
+                }
+                for (byte n = 0; n < k; n++)
+                {
+                    WrapPanel WP = new WrapPanel { HorizontalAlignment = HorizontalAlignment.Center, Margin = new Thickness(0, 4, 0, 0) };
+                    {
+                        WP.Children.Add(new TextBlock { Foreground = Brushes.White, Text = "GPU" + n });
+                        CheckBox CB = new CheckBox { Name = "g" + n.ToString(), IsChecked = PM.Profile.GPUsSwitch[n] };
+                        CB.Checked += GPUCB_Checked;
+                        CB.Unchecked += GPUCB_Unchecked;
+                        WP.Children.Add(CB);
+                    }
+                    GPUswitchSP.Children.Add(WP);
+                }
+            }
+            PM.SaveProfile();
+        }
+        private void GPUCB_Checked(object sender, RoutedEventArgs e)
+        {
+            PM.Profile.GPUsSwitch[Convert.ToInt32(((CheckBox)sender).Name.ToCharArray()[1].ToString())] = true;
+            PM.SaveProfile();
+        }
+        private void GPUCB_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PM.Profile.GPUsSwitch[Convert.ToInt32(((CheckBox)sender).Name.ToCharArray()[1].ToString())] = false;
+            PM.SaveProfile();
+        }
+        #endregion
+    }
+}
